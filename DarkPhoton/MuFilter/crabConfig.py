f2617bfd9ca5fd6f4a7f2a38e5ad91c36351f6e6
Michael Revering <michael.martin.r@cern.ch>
2021-03-11 10:48:36 -0600

    Modified crabconfig and python config file to use lumi based splitting.

--- DarkPhoton/MuFilter/crabConfig.py
+++ DarkPhoton/MuFilter/crabConfig.py
@@ -1,23 +1,24 @@
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'Dphoton_MuPlusXSkim_WJets'
+config.General.requestName = 'Dphoton_MuPlusXSkim_Run2018B_lumisplit'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'python/ConfFile_MuPlusX_AOD_cfg.py'
-config.JobType.maxMemoryMB = 5000
+config.JobType.maxMemoryMB = 2500
 config.JobType.allowUndistributedCMSSW = True
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
+config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
 config.Data.outLFNDirBase = '/store/user/mireveri/MuPlusX/'
-#config.Data.inputDataset  '/SingleMuon/Run2018D-22Jan2019-v2/AOD'
-config.Data.userInputFiles = open('/uscms_data/d3/revering/CMSSW_10_6_17_patch1/src/DarkPhoton/MuFilter/WJetsToLNu.txt').readlines()
+config.Data.inputDataset = '/SingleMuon/Run2018B-12Nov2019_UL2018-v2/AOD'
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
+#config.Data.userInputFiles = open('/uscms_data/d3/revering/MuPxFilter/CMSSW_10_6_17_patch1/src/DarkPhoton/MuFilter/Run2018C.txt').readlines()
 config.Data.publication = False
-config.Data.outputDatasetTag = 'WJets'
+config.Data.outputDatasetTag = 'Run2018B'
 config.Data.allowNonValidInputDataset = True
 
 config.Site.blacklist = ['T0_*']
